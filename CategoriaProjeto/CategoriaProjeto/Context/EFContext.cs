5a418763333401efb9ff5df99f31102851f0de19
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2022-05-23 07:42:24 -0300

    Projeto Categoria

---
+++ CategoriaProjeto/CategoriaProjeto/Context/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using CategoriaProjeto.Models;
+
+namespace CategoriaProjeto.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp Net MVC CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+
+    }
+}

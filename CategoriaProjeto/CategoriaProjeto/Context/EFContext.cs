1d12bda809fcde8ac4a764a83899520133f36851
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2022-05-23 08:19:17 -0300

    EF

--- CategoriaProjeto/CategoriaProjeto/Context/EFContext.cs
+++ CategoriaProjeto/CategoriaProjeto/Context/EFContext.cs
@@ -9,7 +9,7 @@ namespace CategoriaProjeto.Context
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Asp Net MVC CS") { }
+        public EFContext() : base("Asp_Net_MVC_CS") { }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
 
